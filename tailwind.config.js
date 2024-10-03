7cc9d782ae4351873a76f36f61712b641af50130
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-10-04 01:18:36 +0300

    Configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/*.ejs"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Inria_Sans', 'sans'],
+      serif: ['Inria_Serif', 'serif'],
+      inika: 'Inika'
+    }
+  },
+  plugins: [],
+}
