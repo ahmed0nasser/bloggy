8de62f0c2c41697ff5751b8efee8c6d4967e1748
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-10-05 01:23:57 +0300

    Add custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
       sans: ['Inria_Sans', 'sans'],
       serif: ['Inria_Serif', 'serif'],
       inika: 'Inika'
+    },
+    colors: {
+      white: '#FFFFFF',
+      'primary': '#C7253E',
+      'secondary': '#821131',
+      'grey-light': '#656565',
+      'grey-heavy': '#4B4B4B',
     }
   },
   plugins: [],
