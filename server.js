bf438f596975b1ceeb09d0aa7dd355505ab8b57e
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-10-07 22:51:03 +0300

    Add blogs page route

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require("express")
 
 const app = express()
 const user = {name: '123456789_123456789_', imgSrc: "data:image/jpeg;base64," + fs.readFileSync("./public/imgs/author.jpeg", {encoding: 'base64'})}
+const blogEntry = {id:1,title:"Blog Title", date:"Publishing date", description:"blog description you wanna write"}
+const blogEntries = []
+for (let i = 0; i < 5; i++) {
+    blogEntries.push(blogEntry)
+}
 
 // Template Engine
 app.set('view engine', 'ejs')
@@ -16,6 +21,11 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+app.get("/blogs", (req, res) => {
+    res.render("blogs", {user, blogEntries})
+    // res.render("blogs")
+})
+
 app.get("/login", (req, res) => {
     res.render("login")
 })
@@ -23,4 +33,5 @@ app.get("/login", (req, res) => {
 app.get("/signup", (req, res) => {
     res.render("signup")
 })
+
 app.listen(5000)
