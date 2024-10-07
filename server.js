1310b5aa09db39c2ad8eda7c2bc40c314cb8888e
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-10-07 22:29:49 +0300

    Set view engine & add home page route

--- server.js
+++ server.js
@@ -2,5 +2,18 @@ const fs = require('fs')
 const express = require("express")
 
 const app = express()
+const user = {name: '123456789_123456789_', imgSrc: "data:image/jpeg;base64," + fs.readFileSync("./public/imgs/author.jpeg", {encoding: 'base64'})}
+
+// Template Engine
+app.set('view engine', 'ejs')
+
+// Middleware
+app.use(express.static("public"))
+
+// Routes
+app.get("/", (req, res) => {
+    // res.render("index", {user})
+    res.render("index")
+})
 
 app.listen(5000)
