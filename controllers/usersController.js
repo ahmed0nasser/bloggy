f26b872e474de502bcb09065cfd7d8f818b24b29
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-10-31 20:58:59 +0300

    Add users creation and fetching functionality

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,29 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const Counter = require("../models/Counter");
+// Constants
+const SALT_ROUNDS = 10;
 
-async function addNewUser(username, password) {
-  const hashedPassword = await bcrypt.hash(password, process.env.SALT_ROUNDS);
-  const numUsers = await User.estimatedDocumentCount();
+async function createNewUser(username, password) {
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  const userCounter = await Counter.findById("user");
+  const userId = await userCounter.getNextSequenceValue();
   await User.create({
-    _id: numUsers + 1,
+    _id: userId,
     name: username,
     password: hashedPassword,
   });
+  return userId;
 }
 
-module.exports = { addNewUser };
+async function createNewAdmin(username, password) {
+  const userId = await createNewUser(username, password);
+  await User.findByIdAndUpdate(userId, { isAdmin: true });
+  return userId;
+}
+
+async function getUserByName(username) {
+  return await User.findOne({ name: username });
+}
+
+module.exports = { createNewUser, createNewAdmin, getUserByName };
