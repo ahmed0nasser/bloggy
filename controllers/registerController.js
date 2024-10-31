d69e70113233e1b9d43b0c3c59c8215d4f13c842
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-10-31 21:24:49 +0300

    Rename signupController to registerController & add some functionality

---
+++ controllers/registerController.js
@@ -0,0 +1,33 @@
+const { validateUserInfo } = require("./validationController");
+const { createNewSession } = require("./sessionController");
+const usersController = require("./usersController");
+// Errors
+const RequestError = require("../Errors/RequestError");
+const UnexpectedError = require("../Errors/UnexpectedError");
+const SameUserError = require("../Errors/SameUserError");
+// Constants
+const SESSION_COOKIE = {
+  name: "sessionId",
+  options: {
+    httpOnly: true,
+    secure: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+};
+
+async function handleSignup(req, res, next) {
+  const { username, password } = req.body;
+  try {
+    validateUserInfo(username, password);
+    const sameUsername = await usersController.getUserByName(username);
+    if (sameUsername) throw new SameUserError();
+    const userId = await usersController.createNewUser(username, password);
+    const sessionId = await createNewSession(userId);
+    res.cookie(SESSION_COOKIE.name, sessionId, SESSION_COOKIE.options);
+  } catch (error) {
+    req.popup = error instanceof RequestError ? error : new UnexpectedError();
+  }
+  next();
+}
+
+module.exports = { handleSignup };
