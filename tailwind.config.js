8c28316990d43ebd85cb7f58e745a5aeb488ba10
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-11-08 23:30:20 +0200

    Use original tailwindcss colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.ejs"],
+  content: ["./views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        'primary': '#C7253E',
+        'secondary': '#821131',
+        'grey-light': '#656565',
+        'grey-heavy': '#4B4B4B',
+      }
+    },
     fontFamily: {
       sans: ['Inria_Sans', 'sans'],
       serif: ['Inria_Serif', 'serif'],
       inika: 'Inika'
     },
-    colors: {
-      white: '#FFFFFF',
-      'primary': '#C7253E',
-      'secondary': '#821131',
-      'grey-light': '#656565',
-      'grey-heavy': '#4B4B4B',
-    }
   },
   plugins: [],
 }
