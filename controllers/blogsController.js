42ccb23505f1e60bc702b0f09924691336553f96
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-11-08 16:54:16 +0200

    Add Blog CRUD functions

---
+++ controllers/blogsController.js
@@ -0,0 +1,35 @@
+const Blog = require("../models/Blog");
+const Counter = require("../models/Counter");
+const utils = require("../utils/utils");
+// Errors
+const RequestError = require("../errors/RequestError");
+const UnexpectedError = require("../errors/UnexpectedError");
+
+async function getBlog(blogId) {
+  return await Blog.findById(blogId);
+}
+
+async function getAllBlogs() {
+  return await Blog.find({});
+}
+
+async function createNewBlog({ title, body, summary }) {
+  const blogCounter = await Counter.findById("blog");
+  const blogId = await blogCounter.getNextSequenceValue();
+  const date = utils.formatBlogDate(Date.now());
+  const uri = utils.constructBlogUri(title, blogId);
+  await Blog.create({ _id: blogId, title, body, summary, date, uri });
+  return blogId;
+}
+
+async function updateBlog(blogId, { title, body, summary }) {
+  const date = utils.formatBlogDate(Date.now());
+  const uri = utils.constructBlogUri(title, blogId);
+  await Blog.findByIdAndUpdate(blogId, { title, body, summary, date, uri });
+  return blogId;
+}
+
+async function deleteBlog(blogId) {
+  await Blog.findByIdAndDelete(blogId);
+  return blogId;
+}
