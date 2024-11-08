9c24d83c5dabd1cf236fe83b0a3bde5c92f44ba4
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-11-08 16:58:00 +0200

    Add Comment CRUD functions

---
+++ controllers/commentsController.js
@@ -0,0 +1,34 @@
+const utils = require("../utils/utils");
+// Errors
+const RequestError = require("../errors/RequestError");
+const UnauthenticatedError = require("../errors/UnauthenticatedError");
+const UnauthorizedError = require("../errors/UnauthorizedError");
+const UnexpectedError = require("../errors/UnexpectedError");
+
+async function createNewComment(blog, commentAuthor, commentBody) {
+  const author = { name: commentAuthor.name, img: commentAuthor.img };
+  const date = utils.formatBlogDate(Date.now());
+  blog.comments.unshift({ author, date, body: commentBody });
+  await blog.save();
+}
+
+async function updateComment(blog, commentIndex, commentBody) {
+  const comment = blog.comments[commentIndex];
+  comment.isEdited = true;
+  comment.body = commentBody;
+  comment.date = Date.now();
+  await blog.save();
+}
+
+async function deleteComment(blog, commentIndex) {
+  blog.comments.splice(commentIndex, 1);
+  await blog.save();
+}
+
+async function toggleCommentLike(blog, commentIndex, likerName) {
+  const { likes } = blog.comments[commentIndex];
+  const likerIndex = likes.indexOf(likerName);
+  likerIndex === -1 ? likes.push(likerName) : likes.splice(likerIndex, 1);
+  await blog.save();
+}
+
