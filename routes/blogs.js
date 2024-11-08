27601f52b63d45460ba35c3ce5cab586d0ba5727
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-11-08 17:04:22 +0200

    Add blogs endpoints

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,28 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const blogsController = require("../controllers/blogsController");
 
-const blogEntry = {
-  id: 1,
-  title: "Blog Title",
-  date: "Publishing date",
-  description: "blog description you wanna write",
-};
-const blogEntries = [];
-for (let i = 0; i < 5; i++) {
-  blogEntries.push(blogEntry);
-}
-
-router.get("/", (req, res) => {
-  res.render("pages/blogs", { user: req.user, blogEntries });
+router.get("/", async (req, res) => {
+  const blogs = await blogsController.getAllBlogs();
+  res.render("pages/blogs", { user: req.user, blogs });
 });
 
-router.get("/:blogTitle/:blogId", (req, res) => {
-  // #TODO: fetch blog with id=blogId from database
-  res.render("pages/blog", { user: req.user });
-});
+// # TODO: refuse unauthorized access
+router
+  .route("/new")
+  .get((req, res) => {
+    res.render("pages/blogEdit", { user: req.user });
+  })
+  .post(blogsController.handleNewBlog, (req, res) => {
+    req.popup
+      ? res.render("pages/blogEdit", { popup: req.popup })
+      : res.redirect("/blogs");
+  });
 
-router.get("/:blogTitle/:blogId/edit", (req, res) => {
-  res.render("pages/blogEdit", { user: req.user });
+router
+  .route("/:blogUri/edit")
+  .get(blogsController.handleBlogFetch, (req, res) => {
+    res.render("pages/blogEdit", { user: req.user, blog: req.blog });
+  })
+  .post(blogsController.handleBlogEdit, (req, res) => {
+    req.popup
+      ? res.render("pages/blogEdit", { popup: req.popup })
+      : res.redirect("/blogs");
+  });
+
+router.post(
+  "/:blogUri/delete",
+  blogsController.handleBlogDelete,
+  (req, res) => {
+    res.redirect("/blogs");
+  }
+);
+
+router.use("/:blogUri/comments", blogsController.handleBlogFetch, require("./comments"))
+
+router.get("/:blogUri", blogsController.handleBlogFetch, (req, res) => {
+  req.popup
+    ? res.render("pages/blogs", { popup: req.popup })
+    : res.render("pages/blog", { user: req.user, blog: req.blog });
 });
 
 module.exports = router;
