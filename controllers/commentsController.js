2e69015c95c9543225d659f268a2e31204cca833
ahmed0nasser <ahmed.n.abulmajeed@gmail.com>
2024-11-08 16:58:22 +0200

    Add Comment request handlers

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -5,6 +5,81 @@ const UnauthenticatedError = require("../errors/UnauthenticatedError");
 const UnauthorizedError = require("../errors/UnauthorizedError");
 const UnexpectedError = require("../errors/UnexpectedError");
 
+// #TODO: comment validation
+
+async function handleNewComment(req, res, next) {
+  try {
+    const { comment: commentBody } = req.body;
+    if (!commentBody) throw new RequestError("Comment must not be empty", 400);
+    if (!req.user) throw new UnauthenticatedError();
+    await createNewComment(req.blog, req.user, commentBody);
+  } catch (error) {
+    req.popup = error instanceof RequestError ? error : new UnexpectedError();
+  }
+  next();
+}
+
+async function handleCommentEdit(req, res, next) {
+  try {
+    const { comment: commentBody } = req.body;
+    if (!commentBody) throw new RequestError("Comment must not be empty", 400);
+    if (!req.user) throw new UnauthenticatedError();
+    if (
+      !(
+        req.user.name === req.blog.comments[req.params.commentIndex].author.name
+      )
+    )
+      throw new UnauthorizedError();
+    if (
+      req.params.commentIndex < 0 ||
+      req.params.commentIndex >= req.blog.comments.length
+    )
+      throw new RequestError("Invalid Comment Index", 400);
+    await updateComment(req.blog, req.params.commentIndex, commentBody);
+  } catch (error) {
+    req.popup = error instanceof RequestError ? error : new UnexpectedError();
+  }
+  next();
+}
+
+async function handleCommentDelete(req, res, next) {
+  try {
+    if (!req.user) throw new UnauthenticatedError();
+    if (
+      !req.user.isAdmin &&
+      !(
+        req.user.name === req.blog.comments[req.params.commentIndex].author.name
+      )
+    )
+      throw new UnauthorizedError();
+    if (
+      req.params.commentIndex < 0 ||
+      req.params.commentIndex >= req.blog.comments.length
+    )
+      throw new RequestError("Invalid Comment Index", 400);
+    await deleteComment(req.blog, req.params.commentIndex);
+  } catch (error) {
+    req.popup = error instanceof RequestError ? error : new UnexpectedError();
+  }
+  next();
+}
+
+async function handleCommentLike(req, res, next) {
+  try {
+    if (!req.user) throw new UnauthenticatedError();
+    if (
+      req.params.commentIndex < 0 ||
+      req.params.commentIndex >= req.blog.comments.length
+    )
+      throw new RequestError("Invalid Comment Index", 400);
+    await toggleCommentLike(req.blog, req.params.commentIndex, req.user.name);
+    await req.blog.save();
+  } catch (error) {
+    req.popup = error instanceof RequestError ? error : new UnexpectedError();
+  }
+  next();
+}
+
 async function createNewComment(blog, commentAuthor, commentBody) {
   const author = { name: commentAuthor.name, img: commentAuthor.img };
   const date = utils.formatBlogDate(Date.now());
@@ -32,3 +107,9 @@ async function toggleCommentLike(blog, commentIndex, likerName) {
   await blog.save();
 }
 
+module.exports = {
+  handleNewComment,
+  handleCommentEdit,
+  handleCommentDelete,
+  handleCommentLike,
+};
