3519ab394cd68444ca9c728a811f223fc131c165
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2024-12-10 00:54:21 +0200

    Update after controllers refactor

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-const usersController = require("../controllers/usersController");
+const User = require("../models/User");
 
 module.exports = () => {
   setImmediate(async () => {
     // Connect to DB
     await mongoose.connect(process.env.DB_CONNECTION_STRING);
     // Create admin if not exists
-    if (!(await usersController.getUserByName(process.env.ADMIN_USERNAME)))
-      await usersController.createNewAdmin(
+    if (!(await User.getUserByName(process.env.ADMIN_USERNAME)))
+      await User.createNewAdmin(
         process.env.ADMIN_USERNAME,
         process.env.ADMIN_PASSWORD
       );
